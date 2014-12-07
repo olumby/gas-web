5d59855afb48d3d43d5366f116a23822d94dc799
Oliver Lumby <me@oliverlumby.com>
2014-12-07 23:39:34 +0000

    Add php docblocks

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -6,16 +6,31 @@ use Illuminate\Support\Facades\Cache;
 
 class ApiController extends BaseController {
 
-	protected $prices;
+	/**
+	 * Price instance.
+	 *
+	 * @var
+	 */
+	protected $price;
 
+	/**
+	 * @param Price $price
+	 */
 	function __construct(Price $price)
 	{
 		$this->price = $price;
 	}
-	
+
+	/**
+	 * Return all prices for a fuel type.
+	 *
+	 * @param $format (json|geojson)
+	 * @param $name
+	 * @return array
+	 */
 	public function prices($format, $name)
 	{
-		$query = Cache::remember("$format-$name", 20, function() use($name)
+		$query = Cache::remember("$format-$name", 20, function () use ($name)
 		{
 			return $this->price->ofType($name)->get()->toJson();
 		});
@@ -28,9 +43,19 @@ class ApiController extends BaseController {
 		return $query;
 	}
 
+	/**
+	 * Return prices for a fuel type and geographical region.
+	 *
+	 * @param $format (json|geojson)
+	 * @param $name
+	 * @param $lat
+	 * @param $lng
+	 * @param $prox
+	 * @return array
+	 */
 	public function geoPrices($format, $name, $lat, $lng, $prox)
 	{
-		$query = Cache::remember("$format-$name-$lat-$lng-$prox", 20, function() use($name, $lat, $lng, $prox)
+		$query = Cache::remember("$format-$name-$lat-$lng-$prox", 20, function () use ($name, $lat, $lng, $prox)
 		{
 			return $this->price->ofType($name)->closeTo($lat, $lng, $prox)->take(10)->get()->toJson();
 		});
