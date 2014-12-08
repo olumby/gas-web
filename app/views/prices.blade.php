69cb2365dda9e6483cc98c5d61f1d55349ee7ea4
Oliver Lumby <me@oliverlumby.com>
2014-12-08 13:14:49 +0000

    Frontend markers

--- app/views/prices.blade.php
+++ app/views/prices.blade.php
@@ -5,8 +5,6 @@
     <title>Document</title>
 
     <link href='https://api.tiles.mapbox.com/mapbox.js/v2.1.4/mapbox.css' rel='stylesheet' />
-    <link href='https://api.tiles.mapbox.com/mapbox.js/plugins/leaflet-markercluster/v0.4.0/MarkerCluster.css' rel='stylesheet' />
-    <link href='https://api.tiles.mapbox.com/mapbox.js/plugins/leaflet-markercluster/v0.4.0/MarkerCluster.Default.css' rel='stylesheet' />
     <link href='/css/style.css' rel='stylesheet' />
 </head>
 <body>
@@ -71,8 +69,9 @@
         </div>
     </section>
 
+
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script src='https://api.tiles.mapbox.com/mapbox.js/v2.1.4/mapbox.js'></script>
-<script src='https://api.tiles.mapbox.com/mapbox.js/plugins/leaflet-markercluster/v0.4.0/leaflet.markercluster.js'></script>
 <script src='/js/main.js'></script>
 
 </body>
--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,32 @@
-L.mapbox.accessToken = 'pk.eyJ1Ijoib2x1bWJ5IiwiYSI6IjFDTWM3V2sifQ.KXc-98r7KCMVsboqLE9nFA';
+$(document).ready(function () {
+	L.mapbox.accessToken = 'pk.eyJ1Ijoib2x1bWJ5IiwiYSI6IjFDTWM3V2sifQ.KXc-98r7KCMVsboqLE9nFA';
 
-var map = L.mapbox.map('map', 'olumby.ke3hc51l', {
-	attributionControl: false,
-	infoControl: true
+	var geocoder = L.mapbox.geocoder('mapbox.places-v1');
+	var map = L.mapbox.map('map', 'olumby.ke3hc51l', {
+		attributionControl: false,
+		infoControl: true,
+		center: [40.0, -3.4],
+		zoom: 6
+	});
+
+	var featureLayer = L.mapbox.featureLayer()
+		.setStyle({color: 'red'});
+
+	map.on('zoomend moveend', function () {
+		if (map.getZoom() > 12) {
+			var coor = map.getCenter();
+			var url = "http://gas.dev/api/prices/geojson/GPR/" + coor.lat + "," + coor.lng + ",12";
+			featureLayer.loadURL(url)
+				.addTo(map)
+				.on('ready', function (layer) {
+					this.eachLayer(function (marker) {
+						marker.setIcon(L.mapbox.marker.icon({
+							'marker-color': '#CC0000',
+							'marker-symbol': 'fuel'
+						}));
+						marker.bindPopup("<table><tr><td>Name</td><td>" + marker.feature.properties.name + "</td></tr><tr><td>Price</td><td>&euro;" + marker.feature.properties.price + "</td></tr><tr><td>Hours</td><td>" + marker.feature.properties.hours + "</td></tr></table>")
+					});
+				});
+		}
+	});
 });
-map.setView([40.0, -3.4], 6)
-	.addControl(L.mapbox.geocoderControl('mapbox.places-v1', {
-		keepOpen: false,
-		position: 'topright',
-	}));
-
-var featureLayer = L.mapbox.featureLayer('examples.map-h61e8o8e').on('ready', function (e) {
-
-	var markers = new L.MarkerClusterGroup({
-		spiderfyOnMaxZoom: true,
-		showCoverageOnHover: false,
-		maxClusterRadius: 60,
-		disableClusteringAtZoom: 13,
-	});
-
-	e.target.eachLayer(function (layer) {
-		markers.addLayer(layer);
-	});
-	map.addLayer(markers);
-
-})
-	.loadURL('http://gas.dev/api/prices/geojson/GPR');
